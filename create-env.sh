f2a54a0c64b5feb5d71409085b2edab57bc51621
C-EEE ORG <146799720+ceeedevops@users.noreply.github.com>
2024-01-25 19:08:12 +0530

    Update create-env.sh

--- create-env.sh
+++ create-env.sh
@@ -2,3 +2,4 @@
 
 echo "MYSQL_ROOT_PASSWOR=$(cat /dev/urandom | strings | grep -Eoa "[a-zA-Z0-9@$%&_]*" | head -n 10 | tr -d '\n'| cut -c1-16)" > .env
 echo "PMM_PASSWOR=$(cat /dev/urandom | strings | grep -Eoa "[a-zA-Z0-9@$%&_]*" | head -n 10 | tr -d '\n'| cut -c1-16)" >> .env
+echo "TZ=Asia/Colombo" >> .env
