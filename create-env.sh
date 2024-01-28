c6c3d4358163e51706be73c1f1c8d29a3e11237d
C-EEE ORG <146799720+ceeedevops@users.noreply.github.com>
2024-01-28 06:48:26 +0530

    Update create-env.sh

--- create-env.sh
+++ create-env.sh
@@ -1,7 +1,8 @@
 #/usr/bin/bash
 
-echo "ROOT_ACCESS_PASSWORD=$(cat /dev/urandom | strings | grep -Eoa "[a-zA-Z0-9@$%&_]*" | head -n 10 | tr -d '\n'| cut -c1-16)" > .env
-echo "DATABASE_PASSWORD=$(cat /dev/urandom | strings | grep -Eoa "[a-zA-Z0-9@$%&_]*" | head -n 10 | tr -d '\n'| cut -c1-16)" >> .env
+echo "COMPOSE_PROJECT_NAME=mysql-dev" > .env
+echo "ROOT_ACCESS_PASSWORD=$(cat /dev/urandom | strings | grep -Eoa "[a-zA-Z0-9@$%&_]*" | head -n 10 | tr -d '\n'| cut -c1-16)" >> .env
+echo "DATABASE_PASSWORD=$(cat /dev/urndom | strings | grep -Eoa "[a-zA-Z0-9@$%&_]*" | head -n 10 | tr -d '\n'| cut -c1-16)" >> .env
 echo "USER_DB_NAME=zerodb" >> .env  
 echo "MYSQL_USER=zerodba" >> .env
 echo "TZ=Asia/Colombo" >> .env
